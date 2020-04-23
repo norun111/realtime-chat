2ddfc618a6c8d62078037b036437e79337c5a65e
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-23 20:16:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # rootはRoom一覧画面にしておく
+   root 'rooms#index'
+
+   # resourcesを使うとRESTfulなURLを自動生成できる
+   resources :rooms, only: %i[new show]
+
 end

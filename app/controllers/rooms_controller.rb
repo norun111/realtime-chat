7ccbf4ad4727967762e6f140a69aeace7db821d5
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-23 20:16:50 +0900

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,17 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @rooms = Room.all.order(:id)
+  end
+
+  def new
+    Room.create!
+    redirect_to root_path
+  end
+
   def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages.order(:created_at)
   end
 end
